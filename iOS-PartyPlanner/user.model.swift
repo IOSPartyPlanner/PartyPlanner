5efce7ad937402ff01efbabe907a0714fe27ee59
Bharath Reddy <bharathmh@gmail.com>
2017-04-25 00:26:37 -0700

    Bharath: User model

--- iOS-PartyPlanner/user.model.swift
+++ iOS-PartyPlanner/user.model.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+class User: NSObject {
+
+  var id: String?
+  var userName: String?
+  var passwordHash: String?
+  var name: String?
+  var email: String?
+  var phone: String?
+  var address: Date?
+  var imageUrl: URL?
+  var authType: userAuthEnum?
+  var authToken: String?
+  // var favEvents: [EventIds]
+}
+  
