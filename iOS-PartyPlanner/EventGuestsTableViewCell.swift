80eeb93082b296e0eb6796c264b9ceb5d9a107d5
billyean <tristan.yim@gmail.com>
2017-04-25 19:43:31 -0700

    Add more view controller

---
+++ iOS-PartyPlanner/EventGuestsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventGuestsTableViewCell.swift
+//  iOS-PartyPlanner
+//
+//  Created by Yan, Tristan on 4/25/17.
+//  Copyright © 2017 PartyDevs. All rights reserved.
+//
+
+import UIKit
+
+class EventGuestsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
