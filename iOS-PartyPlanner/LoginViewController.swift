9e5a23e88a3233318303c5ea5f7f5534c4991d7e
Anusha Kopparam <una.020@gmail.com>
2017-04-30 14:22:50 -0700

    Login update

--- iOS-PartyPlanner/LoginViewController.swift
+++ iOS-PartyPlanner/LoginViewController.swift
@@ -28,14 +28,12 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDeleg
     
     view.addSubview(gLoginButton)
     gLoginButton.addTarget(self, action: #selector(handleGLogin), for: .touchUpInside)
-    GIDSignIn.sharedInstance().uiDelegate = self
-    
     
     view.addSubview(tLoginButton)
     tLoginButton.addTarget(self, action: #selector(handleTLogin), for: .touchUpInside)
     
+    GIDSignIn.sharedInstance().uiDelegate = self
     GIDSignIn.sharedInstance().delegate = self
-   
   }
     
     func handleTLogin() {
@@ -45,66 +43,63 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDeleg
                 print("Twitter login failed", error ?? "")
                 return
             }
-            
+//            session.useremail
             guard let token = session?.authToken else {return}
             guard let secret = session?.authTokenSecret else { return }
-            
             let credentials = FIRTwitterAuthProvider.credential(withToken: token, secret: secret)
             
-            FIRAuth.auth()?.signIn(with: credentials, completion: { (user, error) in
-                if error != nil {
-                    print("Failed to login to firebase with twitter", error ?? "")
-                    return
-                }
-                print("Successful login to firebase with twitter", user ?? "")
-                
-            })
+            self.signIntoFirebase(credentials: credentials)
             self.performSegue(withIdentifier: "EventViewSegue", sender: self)
         }
+        
+        let client = TWTRAPIClient.withCurrentUser()
+        let request = client.urlRequest(withMethod: "GET",
+                                        url: "https://api.twitter.com/1.1/account/verify_credentials.json",
+                                                  parameters: ["include_email": "true", "skip_status": "true"],
+                                                  error: nil)
+        
+        client.sendTwitterRequest(request) { response, data, connectionError in
+            print(data)
+            print(response)
+        }
+        
+//        print(request)
     }
     
-    func handleGLogin(){
-        GIDSignIn.sharedInstance().signIn()
-    }
-    
+
     func handleFbLogin(){
         FBSDKLoginManager().logIn(withReadPermissions: ["email", "public_profile"], from: self) { (result, error) in
             if error != nil {
                 print("FB login failed", error ?? "")
                 return
             }
-            self.showEmail()
+            
+            let accessToken = FBSDKAccessToken.current()
+            guard let accessTokenString = accessToken?.tokenString else {return}
+            let credentials = FIRFacebookAuthProvider.credential(withAccessToken: accessTokenString)
+
+            self.signIntoFirebase(credentials: credentials)
+//            self.showEmail()
             self.performSegue(withIdentifier: "EventViewSegue", sender: self)
         }
     }
 
-    func showEmail(){
-        
-        let accessToken = FBSDKAccessToken.current()
-        guard let accessTokenString = accessToken?.tokenString else {
-            return
-        }
-    
-        let credentials = FIRFacebookAuthProvider.credential(withAccessToken: accessTokenString)
-    
-        FIRAuth.auth()?.signIn(with: credentials, completion: { (user, error) in
-            if error != nil {
-                print("FIREBase fb addition error", error ?? "")
-                return
-            }
-            print("Successful logged into firebase", user ?? "")
-        })
-    
-        
-        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start { (connection, result, error) in
-            if error != nil {
-                print("FBSDKGraphRequest failed", error ?? "")
-                return
-            }
-            print(result ?? "")
-        }
-    }
+//    func showEmail(){
+//        
+//        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start { (connection, result, error) in
+//            if error != nil {
+//                print("FBSDKGraphRequest failed", error ?? "")
+//                return
+//            }
+//            print(result ?? "")
+//        }
+//    }
 
+    func handleGLogin(){
+        GIDSignIn.sharedInstance().signIn()
+    }
+    
+    
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         if error != nil {
             print(error)
@@ -117,21 +112,78 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDeleg
         guard let authentication = user.authentication else { return }
         let credential = FIRGoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
         
-        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
-            if error != nil {
-                print("Adding gUser to firebase failed", error!)
-            }
-            else{
-                print("Adding gUser to firebase Successful", user?.uid ?? "")
-            }
-        })
-     
+        signIntoFirebase(credentials: credential)
         performSegue(withIdentifier: "EventViewSegue", sender: self)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func signIntoFirebase(credentials: FIRAuthCredential){
+        FIRAuth.auth()?.signIn(with: credentials, completion: { (user, error) in
+            if error != nil {
+                print("Failed to login to firebase", error ?? "")
+                return
+            }
+            print("Successful login to firebase", user ?? "")
+            print(user?.uid)
+            print(user?.providerID)
+            print(user?.displayName)
+            print(user?.photoURL)
+            print(user?.email)
+        })
+
+    }
+    
+//    
+//    var id: String?
+//    
+//    var userName: String?
+//    
+//    var passwordHash: String?
+//    
+//    var name: String?
+//    
+//    var email: String?
+//    
+//    var phone: String?
+//    
+//    var address: Date?
+//    
+//    var imageUrl: URL?
+//    
+//    var authType: AuthenticationType?
+//    
+//    var authToken: String?
+//    
+    /*public var providerID: String { get }
+     
+     
+     /** @property uid
+     @brief The provider's user ID for the user.
+     */
+     public var uid: String { get }
+     
+     
+     /** @property displayName
+     @brief The name of the user.
+     */
+     public var displayName: String? { get }
+     
+     
+     /** @property photoURL
+     @brief The URL of the user's profile photo.
+     */
+     public var photoURL: URL? { get }
+     
+     
+     /** @property email
+     @brief The user's email address.
+     */
+     public var email: String? { get }
+
+     */
     
 }
 
