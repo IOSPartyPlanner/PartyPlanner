b1810d7a0a7961b65e2d2dad481ddb0c49179ae6
billyean <tristan.yim@gmail.com>
2017-05-07 21:01:46 -0700

    Refacor storyboard

---
+++ iOS-PartyPlanner/EventTasksTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTasksTableViewCell.swift
+//  iOS-PartyPlanner
+//
+//  Created by Yan, Tristan on 5/7/17.
+//  Copyright © 2017 PartyDevs. All rights reserved.
+//
+
+import UIKit
+
+class EventTasksTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
