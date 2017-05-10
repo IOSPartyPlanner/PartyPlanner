9549b4673b3307bfc06066fc73b332f85ca5e3ef
Bharath Reddy <bharathmh@gmail.com>
2017-05-09 22:31:10 -0700

    Added Date and Location selection functions

--- iOS-PartyPlanner/EventCreationViewController.swift
+++ iOS-PartyPlanner/EventCreationViewController.swift
@@ -14,11 +14,20 @@ class EventCreationViewController: UIViewController {
   
   var datepicker = UIDatePicker()
   var toolbar = UIToolbar()
+  var currentIndex: Int!
+  
+  var eventStartDateTime: Date!
+  var eventEndDateTime: Date!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     tableView.delegate = self
     tableView.dataSource = self
+    
+    createDatepicker()
+    
+    eventStartDateTime = Date.init()
+    eventEndDateTime = Date.init().addingTimeInterval(60.0)
     
     tableView.rowHeight = UITableViewAutomaticDimension
     tableView.estimatedRowHeight = 200
@@ -36,24 +45,28 @@ class EventCreationViewController: UIViewController {
   }
   
   
-  /*
-   // MARK: - Navigation
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
-}
-
-// MARK: - Text
-extension EventCreationViewController: UITextFieldDelegate {
-  func textFieldDidBeginEditing(_ textField: UITextField) {
-    print("\n textFieldDidBeginEditing \n")
+  
+  // MARK: - Navigation
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "locationSelectionSegue" {
+      let vc = segue.destination as! LocationsViewController
+      vc.delegate = self
+    }
   }
+  
 }
 
-// MARK: - Table
+  // MARK: - Location Picker 
+extension EventCreationViewController: LocationsViewControllerDelegate {
+  
+  func locationsPickedLocation(controller: LocationsViewController, location: String) {
+    print("Addres picked was \(location)")
+  }
+  
+}
+
+  // MARK: - Table
 extension EventCreationViewController: UITableViewDelegate, UITableViewDataSource {
   
   func numberOfSections(in tableView: UITableView) -> Int {
@@ -74,17 +87,18 @@ extension EventCreationViewController: UITableViewDelegate, UITableViewDataSourc
       
     else if indexPath.row == 1 {
       // Event Name label
-      let cell = tableView.dequeueReusableCell(withIdentifier: "TextInputCell", for: indexPath) as! TextInputCell
+      let cell = tableView.dequeueReusableCell(withIdentifier: "TextInputCell2", for: indexPath) as! TextInputCell2
       cell.textInput.leftImage = #imageLiteral(resourceName: "Pen")
       cell.textInput.leftPadding = 40
       cell.textInput.placeholder = "Event Name"
+      cell.indexRow = indexPath.row
       cell.delegate = self
       return cell
     }
       
     else if indexPath.row == 2 {
       // Location
-      let cell = tableView.dequeueReusableCell(withIdentifier: "TextInputCell", for: indexPath) as! TextInputCell
+      let cell = tableView.dequeueReusableCell(withIdentifier: "TextInputCell2", for: indexPath) as! TextInputCell2
       cell.textInput.leftImage = #imageLiteral(resourceName: "Marker")
       cell.textInput.leftPadding = 40
       cell.textInput.placeholder = "Location"
@@ -95,27 +109,28 @@ extension EventCreationViewController: UITableViewDelegate, UITableViewDataSourc
       
     else if indexPath.row == 3 {
       // Start Time
-      let cell = tableView.dequeueReusableCell(withIdentifier: "TextInputCell", for: indexPath) as! TextInputCell
+      let cell = tableView.dequeueReusableCell(withIdentifier: "TextInputCell2", for: indexPath) as! TextInputCell2
       cell.textInput.leftImage = #imageLiteral(resourceName: "TimerEmpty")
       cell.textInput.leftPadding = 40
       cell.textInput.placeholder = "Event Start Date Time"
       cell.indexRow = indexPath.row
-      cell.datePickerHidden = false
-//      cell.textInput.inputView = datepicker
-//      cell.textInput.inputAccessoryView = toolbar
+      cell.textInput.text = Utils.getShortTimeStampStringFromDate(date: eventStartDateTime)
+      cell.textInput.inputView = datepicker
+      cell.textInput.inputAccessoryView = toolbar
       cell.delegate = self
       return cell
     }
       
     else if indexPath.row == 4 {
       // End Time
-      let cell = tableView.dequeueReusableCell(withIdentifier: "TextInputCell", for: indexPath) as! TextInputCell
+      let cell = tableView.dequeueReusableCell(withIdentifier: "TextInputCell2", for: indexPath) as! TextInputCell2
       cell.textInput.leftImage = #imageLiteral(resourceName: "TimerComplete")
       cell.textInput.leftPadding = 40
       cell.textInput.placeholder = "Event End Date Time"
       cell.indexRow = indexPath.row
-//      cell.textInput.inputView = datepicker
-//      cell.textInput.inputAccessoryView = toolbar
+      cell.textInput.text = Utils.getShortTimeStampStringFromDate(date: eventEndDateTime)
+      cell.textInput.inputView = datepicker
+      cell.textInput.inputAccessoryView = toolbar
       cell.delegate = self
       return cell
     }
@@ -131,40 +146,88 @@ extension EventCreationViewController: UITableViewDelegate, UITableViewDataSourc
 
 // MARK: - Date Picker
 extension EventCreationViewController {
-  func showDatePicker() {
-
-    datepicker.datePickerMode = UIDatePickerMode.dateAndTime
-    
-    let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(self.datePickerValueChanged))
-    toolbar.backgroundColor = UIColor.blue
+  //  func showDatePicker() {
+  //
+  //    datepicker.datePickerMode = UIDatePickerMode.dateAndTime
+  //
+  //    let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(self.datePickerValueChanged))
+  //    toolbar.backgroundColor = UIColor.blue
+  //    toolbar.setItems([doneButton], animated: true)
+  //    toolbar.isUserInteractionEnabled = true
+  //
+  //    let pickerSize : CGSize = datepicker.sizeThatFits(CGSize(width: 0, height: 0))
+  //    datepicker.frame = CGRect(x: 0, y: 300, width: pickerSize.width, height: 250)
+  //
+  //    //you probably don't want to set background color as black
+  //    datepicker.backgroundColor = UIColor.gray
+  //    view.addSubview(datepicker)
+  //  }
+  
+  // Mark: - Date Selection
+  func createDatepicker() {
+    toolbar.sizeToFit()
+    let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(datePickerValueChanged))
     toolbar.setItems([doneButton], animated: true)
-    toolbar.isUserInteractionEnabled = true
-    
-    let pickerSize : CGSize = datepicker.sizeThatFits(CGSize(width: 0, height: 0))
-    datepicker.frame = CGRect(x: 0, y: 300, width: pickerSize.width, height: 250)
-
-    //you probably don't want to set background color as black
-    datepicker.backgroundColor = UIColor.gray
-    view.addSubview(datepicker)
   }
   
   func datePickerValueChanged(sender: Any) {
+    view.endEditing(true)
     let dateFormatter = DateFormatter()
     dateFormatter.dateStyle = DateFormatter.Style.medium
     dateFormatter.timeStyle = DateFormatter.Style.medium
     
-    print("\n\n \(datepicker.date) \n")
+    let datetime = datepicker.date
+    
+    // start time selected
+    if currentIndex == 3 {
+      eventStartDateTime = datetime
+      let indexPath = IndexPath(item: currentIndex!, section: 0)
+      tableView.reloadRows(at: [indexPath], with: .automatic)
+      
+    } else if currentIndex == 4 {
+      // end time selected
+      eventEndDateTime = datetime
+      let indexPath = IndexPath(item: currentIndex!, section: 0)
+      tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
   }
-
+  
 }
 
 // MARK: - TextInputCell delegates
-extension EventCreationViewController: TextInputCellDelegate {
-  func textInputCell (textInputCell: TextInputCell, startDateTimeStarted row: Int) {
-//    showDatePicker()
+extension EventCreationViewController: TextInputCell2Delegate {
+  
+  // Event Name filed
+  func textInputCell2(textInputCell2: TextInputCell2, eventNameEntered name: String) {
+    //    currentIndex = 1
+    print(name)
   }
   
-  func textInputCell (textInputCell: TextInputCell, endDateTimeStarted row: Int) {
-//    showDatePicker()
+  // Location Field
+  func textInputCell2(textInputCell2: TextInputCell2, locationInputStarted location: String) {
+    self.performSegue(withIdentifier: "locationSelectionSegue", sender: self)
+    currentIndex = 2
+  }
+  
+  func textInputCell2(textInputCell2: TextInputCell2, locationInputSelected location: String) {
+    
+  }
+  
+  // Start Date Field
+  func textInputCell2(textInputCell2: TextInputCell2, startDateTimeStarted row: Int) {
+    currentIndex  = 3
+  }
+  
+  func textInputCell2(textInputCell2: TextInputCell2, startDateTimeSelected row: Int) {
+    
+  }
+  
+  // End Date Field
+  func textInputCell2(textInputCell2: TextInputCell2, endDateTimeStarted row: Int) {
+    currentIndex = 4
+  }
+  
+  func textInputCell2(textInputCell2: TextInputCell2, endDateTimeSelected row: Int) {
+    
   }
 }
---
+++ iOS-PartyPlanner/TextInputCell2.swift
@@ -0,0 +1,95 @@
+//
+//  TextInputCell2.swift
+//  iOS-PartyPlanner
+//
+//  Created by Bharath D N on 5/9/17.
+//  Copyright © 2017 PartyDevs. All rights reserved.
+//
+
+import UIKit
+
+
+@objc protocol TextInputCell2Delegate {
+  // Event Name
+  @objc optional func textInputCell2(textInputCell2: TextInputCell2, eventNameEntered name: String)
+  
+  // Location
+  @objc optional func textInputCell2(textInputCell2: TextInputCell2, locationInputStarted location: String)
+  @objc optional func textInputCell2(textInputCell2: TextInputCell2, locationInputSelected location: String)
+  
+  // Start Date
+  @objc optional func textInputCell2(textInputCell2: TextInputCell2, startDateTimeStarted row: Int)
+  @objc optional func textInputCell2(textInputCell2: TextInputCell2, startDateTimeSelected row: Int)
+  
+  // End Date
+  @objc optional func textInputCell2(textInputCell2: TextInputCell2, endDateTimeStarted row: Int)
+  @objc optional func textInputCell2(textInputCell2: TextInputCell2, endDateTimeSelected row: Int)
+}
+
+class TextInputCell2: UITableViewCell {
+  
+  @IBOutlet weak var textInput: CustomTextField!
+  
+  var indexRow: Int!
+  weak var delegate: TextInputCell2Delegate?
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+    textInput.delegate = self
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    // Configure the view for the selected state
+  }
+  
+}
+
+extension TextInputCell2: UITextFieldDelegate {
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    if indexRow == 1 {
+      print("you selected 1")
+    }
+    else if indexRow == 2 {
+      print("you selected 2")
+      //TODO: for location open new page
+      delegate?.textInputCell2!(textInputCell2: self, locationInputStarted: "Location")
+    }
+    else if indexRow == 3 {
+      print("you selected Start Date")
+      //      textField.resignFirstResponder()
+      delegate?.textInputCell2!(textInputCell2: self, startDateTimeStarted: indexRow)
+    }
+    else if indexRow == 4 {
+      print("you selected End Date")
+      //      textField.resignFirstResponder()
+      delegate?.textInputCell2!(textInputCell2: self, endDateTimeStarted: indexRow)
+    }
+  }
+  
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    if indexRow == 1 {
+      print("you completed 1")
+      delegate?.textInputCell2!(textInputCell2: self, eventNameEntered: textInput.text!)
+    }
+    else if indexRow == 2 {
+      print("you selected 2")
+      //TODO: for location open new page
+    }
+    else if indexRow == 3 {
+      print("you selected Start Date")
+      //      delegate?.textInputCell!(textInputCell: self, startDateTimeSelected: indexRow)
+    }
+    else if indexRow == 4 {
+      print("you selected End Date")
+      //      delegate?.textInputCell!(textInputCell: self, endDateTimeSelected: indexRow)
+    }
+  }
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return true
+  }
+}
+
